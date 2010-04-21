1e560e6bc5824a652aa7b1c443b2d691d96718b3
llatzhar <llatzhar@gmail.com>
2010-04-21 14:57:14 +0900

    implement some url.

--- tesso.rb
+++ tesso.rb
@@ -2,9 +2,11 @@
 
 $:.unshift('lib')
 
-CONFIG = {
+$CONFIG = {
    :footer_note => 'some note',
-   :footer_url => 'http://google.com/'
+   :footer_url => 'http://google.com/',
+   :users => 'users.yml',
+   :rooms => 'rooms.yml'
 }
 
 require 'sinatra'
@@ -15,28 +17,27 @@ enable :sessions
 $users = nil
 
 helpers do
-   def get_users
-      if $users == nil
-         $users = Users.new
-      end
-      $users
-   end
    def role?(user, rolestr)
-      users = get_users
-      u = users.find_by(user)
+      users = Users.instance($CONFIG[:users])
+      u = users.find(user)
+      return false if u == nil
       u["role"] == rolestr
    end
+   
    def admin?(session)
       return false if session[:user] == nil
       role?(session[:user], "admin")
    end
+   
    def user?(session)
       return false if session[:user] == nil
       role?(session[:user], "user")
    end
+   
    def room?(session)
       return false if session[:user] == nil
-      role?(session[:user], "room")
+      rooms = Rooms.instance($CONFIG[:rooms])
+      return rooms.find(session[:user]) != nil
    end
 end
 
@@ -50,25 +51,29 @@ end
 
 get '/login' do
    session.clear
-   erb :login, :locals => { :constants => CONFIG }
+   erb :login, :locals => { :constants => $CONFIG }
 end
 
 post '/login' do
-   users = Users.new
-   if users.authenticate(params['user'], params['pass'])
+   room = Rooms.instance($CONFIG[:rooms]).authenticate(params['user'], params['pass'])
+   if room != nil
       session[:user] = params['user']
-      user = users.find_by(session[:user])
-      if user["role"] == "admin"
-         redirect './users'
-      elsif user["role"] == "user"
-         redirect './rooms'
-      else
-         redirect './files'
-      end
+      redirect "./#{room["id"]}"
    else
-      @message = 'login failed.'
-      erb :login, :locals => { :constants => CONFIG }
+      users = Users.instance($CONFIG[:users])
+      user = users.authenticate(params['user'], params['pass'])
+      if user != nil
+         session[:user] = params['user']
+         #user = users.find(session[:user])
+         if user["role"] == "admin"
+            redirect './users'
+         elsif user["role"] == "user"
+            redirect './rooms'
+         end
+      end
    end
+   @message = 'login failed.'
+   erb :login, :locals => { :constants => $CONFIG }
 end
 
 get '/logout' do
@@ -88,44 +93,97 @@ end
 
 get '/users' do
    redirect './login' if !admin?(session)
-   erb :users, :locals => { :users => get_users, :constants => CONFIG }
+   erb :users, :locals => {
+      :users => Users.instance($CONFIG[:users]),
+      :constants => $CONFIG
+   }
 end
 
 get '/user/new' do
    redirect '../login' if !admin?(session)
-   erb :user_new, :locals => { :constants => CONFIG }
+   erb :user_new, :locals => {
+      :constants => $CONFIG
+   }
 end
 
 post '/user/new' do
    redirect '../login' if !admin?(session)
-   get_users.add(params)
+   Users.instance($CONFIG[:users]).add(params)
    redirect '../users'
 end
 
 get '/user/:uid/edit' do |uid|
    redirect '../../login' if !admin?(session)
-   erb :user_edit, :locals => { :user => get_users.find_by(uid), :constants => CONFIG }
+   
+   erb :user_edit, :locals => {
+      :user => Users.instance($CONFIG[:users]).find(uid),
+      :constants => $CONFIG
+   }
 end
 
 post '/user/:uid/edit' do |uid|
    redirect '../../login' if !admin?(session)
-   get_users.edit(params)
+   Users.instance($CONFIG[:users]).edit(params)
    redirect '../../users'
 end
 
 get '/user/:uid/delete' do |uid|
    redirect '../../login' if !admin?(session)
-   get_users.delete(uid)
+   Users.instance($CONFIG[:users]).delete(uid)
    redirect '../../users'
 end
 
 get '/rooms' do
-   if session[:user] == nil
-      redirect './login'
-   end
-   users = Users.new
-   if !users.user?(session[:user])
-      redirect './login'
-   end
-   erb :rooms, :locals => { :users => users, :constants => CONFIG }
+   redirect './login' if !user?(session)
+   rooms = Rooms.instance($CONFIG[:rooms])
+   
+   #TODO cannot call h, u method(erb).
+   erb :rooms, :locals => {
+      :user => Users.instance($CONFIG[:users]).find(session[:user]),
+      :rooms => rooms,
+      :constants => $CONFIG
+   }
+end
+
+get '/room/new' do
+   redirect '../login' if !user?(session)
+   erb :room_new, :locals => {
+      :constants => $CONFIG
+   }
+end
+
+post '/room/new' do
+   redirect '../login' if !user?(session)
+   Rooms.instance($CONFIG[:rooms]).add(params)
+   redirect '../rooms'
+end
+
+get '/:roomid' do |roomid|
+   redirect './login' if !user?(session) and !room?(session)
+   room = Rooms.instance($CONFIG[:rooms]).find(roomid)
+   erb :files, :locals => {
+      :room => room,
+      :constants => $CONFIG
+   }
+end
+
+get '/:roomid/edit' do |roomid|
+   redirect '../login' if !user?(session)
+   
+   erb :room_edit, :locals => {
+      :room => Rooms.instance($CONFIG[:rooms]).find(roomid),
+      :constants => $CONFIG
+   }
+end
+
+post '/:roomid/edit' do |roomid|
+   redirect '../login' if !user?(session)
+   Rooms.instance($CONFIG[:rooms]).edit(params)
+   redirect '../rooms'
+end
+
+get '/:roomid/delete' do |roomid|
+   redirect '../login' if !user?(session)
+   Rooms.instance($CONFIG[:rooms]).delete(roomid)
+   redirect '../rooms'
 end
--- users.rb
+++ users.rb
@@ -1,82 +1,126 @@
 require 'yaml_fix'
 require 'nkf'
 
-class Users
+module YamlStore
    
    include Enumerable
    
-   def initialize
-      @users = YAML.load_file('users.yml')
+   def initialize(filename)
+      @filename = filename
+      @objects = YAML.load_file(@filename)
    end
    
    def each
-      @users.each do |u|
-         yield u
+      @objects.each do |o|
+         yield o
       end
    end
    
-   def authenticate(user, pass)
-      @users.each do |u|
-         if user == u["user_id"] && pass == u["pass"]
-            return u
+   def find(id)
+      #p @objects
+      @objects.each do |o|
+         if o["id"] == id
+            return o
          end
       end
-      return nil
-   end
-
-   def find_by(uid)
-      @users.each do |u|
-         if u["user_id"] == uid
-            return u
-         end
-      end
-      return nil
-   end
-   
-   def admin?(uid)
-      u = find_by(uid)
-      u["role"] == "admin"
-   end
-   
-   def user?
-      u = find_by(uid)
-      u["role"] == "user"
-   end
-   
-   def room?
-      raise "not implemented"
-   end
-   
-   def lock
-      f = File.open('users.yml', 'r')
-      f.flock(File::LOCK_SH)
-      # unlockはプロセス死亡時の自動開放に任せる...でいいの？
+      nil
    end
    
    def flush
-      File.open('users.yml', "w") do |file|
+      File.open(@filename, "w") do |file|
          file.flock(File::LOCK_EX)
-         file.write @users.to_yaml
+         file.write @objects.to_yaml
          file.flock(File::LOCK_UN)
       end
    end
    
+   def authenticate(id, pass)
+      @objects.each do |o|
+         if (id == o["id"]) && (pass == o["pass"])
+            return o
+         end
+      end
+      nil
+   end
+end
+
+class Users
+   
+   include YamlStore
+   
+   def initialize(filename)
+      super(filename)
+      #@users = YAML.load_file('users.yml')
+   end
+   
+   def Users.instance(filename)
+      if @instance == nil
+         @instance = new(filename)
+      end
+      @instance
+   end
+   
    def add(params)
       u = {}
-      u['user_id'] = params['id']
+      u['id'] = params['id']
       u['name'] = NKF.nkf('-Ww --cp932', params['name'])
       u['pass'] = params['pass']
       u['note'] = NKF.nkf('-Ww --cp932', params['note'])
       u['role'] = :user
-      u['files'] = []
-      @users << u
+      @objects << u
       
       flush
    end
    
    #TODO edit user_id(with check).
    def edit(params)
-      u = find_by(params['id'])
+      u = find(params['id'])
+      if u == nil
+         return
+      end
+      
+      u['name'] = NKF.nkf('-Ww --cp932', params['name'])
+      u['pass'] = params['pass']
+      u['note'] = NKF.nkf('-Ww --cp932', params['note'])
+      
+      flush
+   end
+   
+   def delete(name)
+      @objects.delete(find(name))
+      flush
+   end
+end
+
+class Rooms
+   include YamlStore
+   
+   def initialize(filename)
+      super(filename)
+   end
+   
+   def Rooms.instance(filename)
+      if @instance == nil
+         @instance = new(filename)
+      end
+      @instance
+   end
+   
+   def add(params)
+      # TODO check id
+      room = {}
+      room['id'] = params['id']
+      room['name'] = NKF.nkf('-Ww --cp932', params['name'])
+      room['pass'] = params['pass']
+      room['note'] = NKF.nkf('-Ww --cp932', params['note'])
+      room['files'] = []
+      @objects << room
+      
+      flush
+   end
+   
+   def edit(params)
+      u = find(params['id'])
       if u == nil
          return
       end
@@ -89,8 +133,7 @@ class Users
    end
    
    def delete(name)
-      @users.delete(find_by(name))
-      
+      @objects.delete(find(name))
       flush
    end
 end
